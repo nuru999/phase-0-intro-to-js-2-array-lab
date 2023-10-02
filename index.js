245111a46634c56d4f9f39636d2447aebbc0303a
nuru999 <70575815+nuru999@users.noreply.github.com>
2023-10-02 12:46:21 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    // Create a new array and copy the original cats into it
+    const newCats = [...cats, name];
+    return newCats;
+}
+
+function prependCat(name) {
+    // Create a new array and copy the original cats into it
+    const newCats = [name, ...cats];
+    return newCats;
+}
+
+function removeLastCat() {
+    // Create a new array and copy all cats except the last one into it
+    const newCats = cats.slice(0, -1);
+    return newCats;
+}
+
+function removeFirstCat() {
+    // Create a new array and copy all cats except the first one into it
+    const newCats = cats.slice(1);
+    return newCats;
+}
